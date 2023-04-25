4134b24d37e498350ec97278a82cfc7addb6b449
Brian J. Pinney <brianj.pinney@gmail.com>
2023-04-25 14:34:26 -0400

    latest -v

---
+++ index.ts
@@ -0,0 +1,42 @@
+
+let age: number = 39;
+
+let firstName: string = "Brian"
+
+let isTall: boolean = true;
+
+let childNames: string [] = ["Billy","Joe","Bob"]
+// console.log(age)
+// console.log(`${firstName} is ${age} years old`) 
+
+if (isTall) {
+    // console.log("and tall")
+} else {
+    // console.log("short")
+}
+
+for (let i: number = 0; i< childNames.length ; i++) {
+    // console.log(childNames[i])
+}
+
+enum taxForm {
+    standerdTaxForm = "1040j",
+    childTaxForm = "641",
+    cryptTaxForm ="420Z"
+}
+
+// console.log(taxForm.standerdTaxForm)
+// console.log(taxForm.cryptTaxForm)
+// console.log(taxForm.standerdTaxForm)
+// console.log(taxForm.childTaxForm)
+
+let zipCode: string | number = 12345
+
+// console.log(zipCode)
+
+function sum( x: number, y: number): number {
+    return x+y
+}
+let total: number = sum(6,7)
+console.log(total)
+
